c7708fc0bd0de6ebde5f76eca8392461a57a9f2d
adarsh <adarshg98765@gmail.com>
2022-07-06 07:18:20 +0530

    Udp Socket using tokio package

--- app/broker/src/main.rs
+++ app/broker/src/main.rs
@@ -1,20 +1,14 @@
-use std::net::UdpSocket;
+use tokio::net::UdpSocket;
+use std::io;
 
-fn main() -> std::io::Result<()> {
-    {
-     let socket = UdpSocket::bind("0.0.0.0:8888")?;
-
-        // Receives a single datagram message on the socket. If `buf` is too small to hold
-        // the message, it will be cut off.
-        let mut buf = [0; 10];
-        println!("{:?}",&mut buf);
-        let (amt, src) = socket.recv_from(&mut buf)?;
-
-        // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-        let buf = &mut buf[..amt];
-        buf.reverse();
-        // println!("{}",&mut buf);
-        socket.send_to(buf, &src)?;
-    } // the socket is closed here
-    Ok(())
+#[tokio::main]
+async fn main() -> io::Result<()>{
+    let sock = UdpSocket::bind("127.0.0.1:8080").await?;
+    let mut buffer = [0;1024];
+    loop{
+        let (len,addr) = sock.recv_from(&mut buffer).await?;
+        println!("{:?} bytrs recieved from {:?}",len,addr);
+        let len = sock.send_to(&buffer[..len],addr).await?;
+        println!("{:?} bytes sent",len);
+    }
 }
--- app/client/src/main.rs
+++ app/client/src/main.rs
@@ -1,16 +1,19 @@
-use std::net::UdpSocket;
-use std::{str,io};
+use tokio::net::UdpSocket;
+use std::io;
 
-fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:8000").expect("Could not bind client socket");
-    socket.connect("127.0.0.1:8888").expect("Could not connect to server");
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let sock = UdpSocket::bind("127.0.0.3:8080").await?;
+
+    let remote_addr = "127.0.0.1:8080";
+    sock.connect(remote_addr).await?;
+    let mut buf = [0; 1024];
+    let str = "hiii";
     loop {
-        let mut input = String::new();
-        let mut buffer = [0u8; 1500];
-        io::stdin().read_line(&mut input).expect("Failed to read from stdin");
-        socket.send(input.as_bytes()).expect("Failed to write to server");
-
-        socket.recv_from(&mut buffer).expect("Could not read into buffer");
-        print!("{}", str::from_utf8(&buffer).expect("Could not write buffer as string"));
+        let len = str.len();
+        let len = sock.send(&buf[..len]).await?;
+        println!("{:?} bytes sent", len);
+        let len = sock.recv(&mut buf).await?;
+        println!("{:?} bytes received from {:?}", len, remote_addr);
     }
 }
